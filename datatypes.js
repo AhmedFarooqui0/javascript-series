f2b52da36e1400096957e95e38bf8a4bcf7b76be
Ahmed Farooqui <farooquiahmed933@gmail.com>
2026-06-26 07:11:09 +0000

    learned variables and data types

---
+++ datatypes.js
@@ -0,0 +1,34 @@
+"use trict"; // it will treat all the js code newer version.
+
+//alert(3+3);  //we are using node not browser it will work when used in browser console.
+console.log(3+3);
+let age=20;
+let name="ahmed";
+let islogin=true;
+  
+//  ===primitive datatype===
+//number==> 2 to power of 53
+// string==>" "
+//boolean==> true/false
+//bigint
+//null==> stand alone value
+//undefined==>
+//symbol==> unique(mostly used in react )
+
+//   ===non primitive datatype===
+//object 
+
+
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof true);
+console.log(typeof object);
+console.log(typeof undefined); //its a type of undefined datatype
+console.log(typeof null);  //null is an object
+
+
+
+
+
+
+
---
+++ variable.js
@@ -0,0 +1,13 @@
+let a=10;                   //we can modify it
+var name="ahmed";  
+var name="md";         //we can modufy it 
+const bankName="SBI";       //we cant modify it once declared
+console.log([a,name,bankName]);   //log() method
+name="farooqui";
+a=20;
+accountCity="jaipur";
+let nothing;
+accountCity="mumbai";
+// bankName="kotak bank";
+console.table([name,a,bankName,accountCity,nothing]);   //table() method
+// var prefered not to use.
