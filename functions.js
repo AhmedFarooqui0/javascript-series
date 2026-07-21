7f8013630fccb87f6669dfb97a0b35de240bfc50
Ahmed Farooqui <farooquiahmed933@gmail.com>
2026-07-21 11:01:12 +0000

    functions and aarrow functions

---
+++ functions.js
@@ -0,0 +1,13 @@
+// arraw functions
+
+const display=(name,age,id)=>{
+console.log(name,age,id);
+};
+// display("ahmed",22,101);
+
+//setTimeout function
+
+// let id=setInterval(display(),2000);
+// clearInterval(id);
+
+setTimeout(display,2000,"ahmed",25,105);
